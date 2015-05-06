d0bbcba391ba1f8a0935cd984012f7a166eac59a
Evgeny <assailant85@mail.ru>
2015-05-07 01:05:57 +0600

    DjonyG/project  Убрал index.php в адресной строке и добавил .html

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ $yii=dirname(__FILE__).'/../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', in_array($_SERVER['REMOTE_ADDR'], [
+    '192.168.4.100',
+    '127.0.0.1',
+    '192.168.4.3',
+    '10.0.2.2'
+]) );
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
